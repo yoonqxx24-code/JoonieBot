118188ee045bb6ff9bd3c3bb7ea55bb47c51c743
yoonqxx24-code <yoonqxx24@gmail.com>
2026-05-14 19:21:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ async function registerCommands() {
   o.setName('card_id')
     .setDescription('Search for a card')
     .setRequired(false)
-),
+)
 .addStringOption(o =>
   o.setName('idol')
     .setDescription('Search by idol')
